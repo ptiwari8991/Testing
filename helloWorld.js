c3a06eb67391ad2481259108ffc7d1c64c7939e7
ptiwari8991 <pankaj.tiwari8991@gmail.com>
2019-10-15 16:28:17 +0530

    lwc hello world components

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,25 @@
 import { LightningElement, track } from 'lwc';
 export default class HelloWorld extends LightningElement {
     @track greeting = 'World';
+
+
+    contacts = [
+        {
+            Id : '003',
+            Name : 'Pankaj',
+            Title : 'Mr.'
+        },
+        {
+            Id : '004',
+            Name : 'Ankur',
+            Title : 'Mr.'
+        },
+        {
+            Id : '005',
+            Name : 'Nikhil',
+            Title : 'Mr.'
+        }
+    ];
     changeHandler(event) {
         this.greeting = event.target.value;
     }
